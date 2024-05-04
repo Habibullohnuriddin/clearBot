2a7cbe896b4ca165f9ed83740ad984ed610318db
Habibullohnuriddin <nuriddinovhabibulloh@gmail.com>
2024-05-04 23:20:16 +0500

    done mission

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ require("./core");
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+app.listen(() => {
+  console.log("Ishlayapti... 😃");
+});
--- controllers/start.js
+++ controllers/start.js
@@ -1,9 +1,37 @@
-const bot = require("../core/bot");
+const { bot } = require("../core/bot");
 
-bot.command("start", async (ctx) => {
-  await ctx.reply(`Hello ${ctx.from.first_name}!`);
+// Botni ishga tushirish
+bot.start(async (ctx) => {
+  ctx.reply(`Salom, ${ctx.message.from.first_name}!`);
 });
 
+// Guruhga yangi a'zo qo'shilganda o'chirish
+bot.on("new_chat_members", async (ctx) => {
+  const newMembers = ctx.message.new_chat_members;
 
+  for (const member of newMembers) {
+    try {
+      await ctx.deleteMessage(member.message_id);
+    } catch (error) {
+      console.error("❌ Xabarni o'chirishda xato:", error);
+    }
+  }
+});
+
+// Guruhdan a'zo chiqib ketganda o'chirish
+bot.on("left_chat_member", async (ctx) => {
+  const leftMember = ctx.message.left_chat_member;
+
+  try {
+    await ctx.deleteMessage(leftMember.message_id);
+  } catch (error) {
+    console.error("❌ Xabarni o'chirishda xato:", error);
+  }
+});
+
+// Errorlarni ushlab turish
+bot.on("polling_error", (error) => {
+  console.error(error);
+});
 
 bot.launch();
